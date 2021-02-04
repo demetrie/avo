c7452cb1b687ebde5a9d58faf299d279cc7b2e97
demetrie <demetrietyler@gmail.com>
2021-02-04 11:32:29 -0800

    Update header.js
    yellow

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#F4EEDD`,
       marginBottom: `1.45rem`,
     }}
   >
